dac865aba4b725eff3c794ca75cfbd57c455025b
Mike Jakuszewski <michael_jakuszewski@hotmail.com>
2022-09-16 11:30:21 -0400

    adding codewar challenge

---
+++ Kyu-8/Even-or-Odd.js
@@ -0,0 +1,6 @@
+// DESCRIPTION:
+// Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+
+//Solution 1
+
+const even_or_odd = number => number % 2 ? 'Odd' : 'Even'
