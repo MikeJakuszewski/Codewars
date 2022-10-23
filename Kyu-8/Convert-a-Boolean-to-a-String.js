52d758bdfb2dfb04141f6bf4bf789aee63d7d44f
Mike Jakuszewski <michael_jakuszewski@hotmail.com>
2022-10-23 19:56:07 -0400

    adding codewars challenge

---
+++ Kyu-8/Convert-a-Boolean-to-a-String.js
@@ -0,0 +1,14 @@
+// Description
+// Implement a function which convert the given boolean value into its string representation.
+
+// Note: Only valid inputs will be given.
+
+//solution 1
+
+const booleanToString = b => b? 'true' : 'false'
+
+// solution 2
+
+function booleanToString(b){
+    return b.toString();
+  }
