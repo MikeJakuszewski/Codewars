6641a4d0538f2690ded5306c41c010e020a6c418
Mike Jakuszewski <michael_jakuszewski@hotmail.com>
2022-10-25 10:00:24 -0400

    adding new codewar challenges

---
+++ Kyu-8/Lost-Without-a-Map.js
@@ -0,0 +1,14 @@
+// Description 
+
+// Given an array of integers, return a new array with each value doubled.
+
+// For example:
+
+// [1, 2, 3] --> [2, 4, 6]
+
+//solution 1
+
+function maps(x){
+    let newArr = x.map( x => x*2)
+    return newArr
+  }
