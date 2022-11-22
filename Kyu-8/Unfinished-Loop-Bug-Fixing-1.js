9ce0a953e9fe87ee0206ee0cdfc424a6388d41e6
Mike Jakuszewski <michael_jakuszewski@hotmail.com>
2022-11-22 09:55:04 -0500

    adding codewar challenges

---
+++ Kyu-8/Unfinished-Loop-Bug-Fixing-1.js
@@ -0,0 +1,15 @@
+// DESCRIPTION:
+// Unfinished Loop - Bug Fixing #1
+// Oh no, Timmy's created an infinite loop! Help Timmy find and fix the bug in his unfinished for loop!
+
+//Solution 1
+
+function createArray(number){
+    let newArray = [];
+    
+    for(let counter = 1; counter <= number; counter++){
+      newArray.push(counter);
+    }
+    
+    return newArray;
+}
