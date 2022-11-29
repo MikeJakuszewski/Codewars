09512f9e27033da08bdd9fb84593ba84dc05efcf
Mike Jakuszewski <michael_jakuszewski@hotmail.com>
2022-11-29 08:46:02 -0500

    adding codewar challenge

--- Kyu-8/Well-of-Ideas-Easy-Version.js
+++ Kyu-8/Well-of-Ideas-Easy-Version.js
@@ -21,3 +21,8 @@ function well(x){
 
 //Solution 2
 
+const well = x => {
+    const good_count = x.filter(x => x == 'good').length;
+    return good_count < 1 ? 'Fail!' : 
+           good_count < 3 ? 'Publish!' : 'I smell a series!';
+  }
