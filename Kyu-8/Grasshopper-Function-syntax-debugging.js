fa4e75dc227c123345287d9e8f79310847592972
Mike Jakuszewski <michael_jakuszewski@hotmail.com>
2022-12-30 10:23:49 -0500

    adding codewar challenge

---
+++ Kyu-8/Grasshopper-Function-syntax-debugging.js
@@ -0,0 +1,12 @@
+// Grasshopper - Function syntax debugging
+// A student was working on a function and made some syntax mistakes while coding. Help them find their mistakes and fix them.
+
+//Soution 1
+
+function main (verb, noun){ 
+    return verb + noun
+}
+
+//Solution 2
+
+const main = (verb,noun) => verb + noun
