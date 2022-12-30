bc1418d3ad8aa959db66683829dcf0c7f3bf0ff0
Mike Jakuszewski <michael_jakuszewski@hotmail.com>
2022-12-29 23:10:57 -0500

    adding codewar challenge

---
+++ Kyu-8/Bin-to-Decimal.js
@@ -0,0 +1,12 @@
+// DESCRIPTION:
+// Complete the function which converts a binary number (given as a string) to a decimal number.
+
+//Solution 1
+
+const binToDec = bin => Number.parseInt(bin,2)
+
+//Solution 2
+
+function binToDec(bin){
+    return parseInt(bin,2);
+}
