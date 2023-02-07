bfe6922a7fba0e62ee5be7ea8531c1e1d8f065be
Mike Jakuszewski <michael_jakuszewski@hotmail.com>
2023-02-06 20:03:02 -0500

    adding codewar challenge

---
+++ Kyu-8/Grasshopper-Variable-Assignment-Debug.js
@@ -0,0 +1,10 @@
+// DESCRIPTION:
+// Variable assignment
+// Fix the variables assigments so that this code stores the string 'devLab' in the variable name.
+
+//Solution 1
+
+var a = "dev"
+var b = "Lab"
+
+var name = a + b
