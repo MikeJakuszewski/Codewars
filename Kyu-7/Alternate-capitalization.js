9f6a787b1f3fc8232f0d16ba77d716a8460e64a3
Mike Jakuszewski <michael_jakuszewski@hotmail.com>
2023-03-10 09:36:43 -0500

    adding codewar challenge

---
+++ Kyu-7/Alternate-capitalization.js
@@ -0,0 +1,28 @@
+// DESCRIPTION:
+// Given a string, capitalize the letters that occupy even indexes and odd indexes separately, and return as shown below. Index 0 will be considered even.
+
+// For example, capitalize("abcdef") = ['AbCdEf', 'aBcDeF']. See test cases for more examples.
+
+// The input will be a lowercase string with no spaces.
+
+// Good luck!
+
+//Solution 1
+
+function capitalize(s){
+    let arr =[]
+    
+    arr.push(s.split('').map((x,i) => !(i%2)? x.toUpperCase(): x).join(''))
+    arr.push(s.split('').map((x,i) => i===0 || !(i%2)? x : x.toUpperCase()).join(''))
+    
+    return arr
+  };
+
+//Solution 2
+
+function capitalize(s){
+    const odd = s.split("").map((l, i) => i % 2 !== 0 ? l.toUpperCase() : l).join("");
+    const even = s.split("").map((l, i) => i % 2 === 0 ? l.toUpperCase() : l).join("");
+    return [even, odd];
+  };
+  
