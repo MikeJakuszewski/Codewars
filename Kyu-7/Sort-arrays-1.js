70e40220f5b6bef306b001d91ae6eb4f51f32cfc
Mike Jakuszewski <michael_jakuszewski@hotmail.com>
2023-03-28 10:09:54 -0400

    adding codewar challenge

---
+++ Kyu-7/Sort-arrays-1.js
@@ -0,0 +1,6 @@
+// DESCRIPTION:
+// Just a simple sorting usage. Create a function that returns the elements of the input-array / list sorted in lexicographical order.
+
+//Solution 1
+
+const sortme = names => names.sort()
