ab7804f04e6bb927e7b69e30dd811cd3ce050aee
Mike Jakuszewski <michael_jakuszewski@hotmail.com>
2023-04-04 11:52:08 -0400

    adding codewar challenge

---
+++ Kyu-7/Digits-explosion.js
@@ -0,0 +1,15 @@
+// Given a string made of digits [0-9], return a string where each digit is repeated a number of times equals to its value.
+
+// Examples
+// explode("312")
+// should return :
+
+// "333122"
+// explode("102269")
+// should return :
+
+// "12222666666999999999"
+
+
+//Solution 1
+const explode = s => s.split('').map(x => x.repeat(x)).join('')
