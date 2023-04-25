55cba567ba1cf0faba784717fcda9e173d87662b
Mike Jakuszewski <michael_jakuszewski@hotmail.com>
2023-04-25 12:51:54 -0400

    adding codewar  challenge

--- Kyu-7/Number-Of-Occurrences.js
+++ Kyu-7/Number-Of-Occurrences.js
@@ -12,3 +12,8 @@
 
 //Solution 1
 
+Object.defineProperty(Array.prototype, 'numberOfOccurrences',{ 
+    value : function numberOfOccurrences(element) {
+      return this.filter(x=> x === element).length
+    }
+  });
