ca7d13d5fbf3c0e123432d3c61b7aa2ee4e76cba
Mike Jakuszewski <michael_jakuszewski@hotmail.com>
2023-05-08 15:57:05 -0400

    adding codewar challenge

--- Kyu-7/Grouping-in-string.js
+++ Kyu-7/Grouping-in-string.js
@@ -13,3 +13,15 @@
 
 //Solution 1
 
+const isConsecutive = str => {
+    const seen = new Set();
+    let latest;
+    
+    for (const char of str)
+      if (!seen.has(char))
+        seen.add(latest = char);
+      else if (latest !== char)
+        return false;
+    
+    return true;
+  }
