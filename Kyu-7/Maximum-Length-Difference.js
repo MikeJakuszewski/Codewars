86ed8d81453c6c149d4280fc953c73a18fdf534d
Mike Jakuszewski <michael_jakuszewski@hotmail.com>
2023-05-31 23:26:07 -0400

    adding codewar challenge

--- Kyu-7/Maximum-Length-Difference.js
+++ Kyu-7/Maximum-Length-Difference.js
@@ -14,3 +14,20 @@
 // output: number as a string
 
 //Solution 1
+
+function mxdiflg(a1, a2) {
+  if (a1.length === 0 || a2.length === 0) {
+    return -1;
+  }
+
+  let maxDiff = 0;
+
+  for (let i = 0; i < a1.length; i++) {
+    for (let j = 0; j < a2.length; j++) {
+      const diff = Math.abs(a1[i].length - a2[j].length);
+      maxDiff = Math.max(maxDiff, diff);
+    }
+  }
+
+  return maxDiff;
+}
