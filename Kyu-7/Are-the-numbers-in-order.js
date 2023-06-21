269e704a40a1fae45857c2f1ba403e0ff97efb2d
Mike Jakuszewski <michael_jakuszewski@hotmail.com>
2023-06-21 13:49:31 -0400

    adding codewar challenge

---
+++ Kyu-7/Are-the-numbers-in-order.js
@@ -0,0 +1,45 @@
+// DESCRIPTION:
+// Are the numbers in order?
+// In this Kata, your function receives an array of integers as input. Your task is to determine whether the numbers are in ascending order. An array is said to be in ascending order if there are no two adjacent integers where the left integer exceeds the right integer in value.
+
+// For the purposes of this Kata, you may assume that all inputs are valid, i.e. arrays containing only integers.
+
+// Note that an array of 0 or 1 integer(s) is automatically considered to be sorted in ascending order since all (zero) adjacent pairs of integers satisfy the condition that the left integer does not exceed the right integer in value.
+
+// For example:
+
+// inAscOrder([1,2,4,7,19]); // returns true
+// inAscOrder([1,2,3,4,5]); // returns true
+// inAscOrder([1,6,10,18,2,4,20]); // returns false
+// inAscOrder([9,8,7,6,5,4,3,2,1]); // returns false because the numbers are in DESCENDING order
+// N.B. If your solution passes all fixed tests but fails at the random tests, make sure you aren't mutating the input array.
+
+//Solution 1
+
+function inAscOrder(arr) {
+  let ass = arr
+    .slice()
+    .sort((a, b) => a - b)
+    .toString();
+
+  if (arr.length <= 1 || arr.toString() == ass) return true;
+  else return false;
+}
+
+//Solution 2
+
+function inAscOrder(arr) {
+  let ass = arr
+    .slice()
+    .sort((a, b) => a - b)
+    .toString();
+  let des = arr
+    .slice()
+    .sort((a, b) => b - a)
+    .toString();
+
+  if (arr.length <= 1) return true;
+  if (arr.toString() == ass) return true;
+  if (arr.toString() == des) return false;
+  if (arr.toString() != ass) return false;
+}
