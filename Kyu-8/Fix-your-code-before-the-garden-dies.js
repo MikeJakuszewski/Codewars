fe367935c6d28feae8ddb232da61ad5015a43561
Mike Jakuszewski <michael_jakuszewski@hotmail.com>
2023-07-19 08:46:54 -0400

    adding codewar challenge

--- Kyu-8/Fix-your-code-before-the-garden-dies.js
+++ Kyu-8/Fix-your-code-before-the-garden-dies.js
@@ -6,7 +6,7 @@
 //Solution 1
 
 function rainAmount(mm) {
-  if (mm <= 40) {
+  if (mm < 40) {
     return `You need to give your plant ${40 - mm}mm of water`;
   } else {
     return "Your plant has had more than enough water for today!";
