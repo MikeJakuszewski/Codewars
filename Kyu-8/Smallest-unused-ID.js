230b6db342d590467769dd50b60c3c4cd88aec45
Mike Jakuszewski <michael_jakuszewski@hotmail.com>
2023-08-04 17:05:04 -0400

    adding solution

--- Kyu-8/Smallest-unused-ID.js
+++ Kyu-8/Smallest-unused-ID.js
@@ -10,3 +10,20 @@
 // Go on and code some pure awesomeness!
 
 //Solution 1
+function nextId(ids) {
+  let idsSort = [...new Set(ids.sort((a, b) => a - b))];
+  for (let i = 0; i < idsSort.length; i++) {
+    if (idsSort[i] != i) {
+      return i;
+    }
+  }
+  return idsSort[idsSort.length - 1] + 1;
+}
+
+//Solution 2
+
+function nextId(ids) {
+  var x = 0;
+  while (ids.includes(x)) x++;
+  return x;
+}
