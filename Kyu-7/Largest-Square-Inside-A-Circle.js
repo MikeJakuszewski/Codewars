76354c5d4f55a32a682cabf34f4e7bb358068d06
Mike Jakuszewski <michael_jakuszewski@hotmail.com>
2023-10-05 09:52:41 -0400

    adding codewar challenge

---
+++ Kyu-7/Largest-Square-Inside-A-Circle.js
@@ -0,0 +1,8 @@
+// DESCRIPTION:
+// Determine the area of the largest square that can fit inside a circle with radius r.
+
+//Solution 1
+
+function areaLargestSquare(r) {
+  return Math.pow(r, 2) * 2;
+}
